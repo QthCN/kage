ecd2bd934a17e1ad370e018e19077e5675cd249e
TianHuan <tianhuan@bingotree.cn>
2016-04-21 17:47:12 +0800

    support numpy lib

---
+++ src/numpy_import.h
@@ -0,0 +1,11 @@
+//
+// Created by thuanqin on 16/4/21.
+//
+
+#ifndef KAGE_COMMON_H
+#define KAGE_COMMON_H
+
+#define NO_IMPORT_ARRAY
+#define PY_ARRAY_UNIQUE_SYMBOL KAGEEXT_ARRAY_API
+
+#endif //KAGE_COMMON_H
